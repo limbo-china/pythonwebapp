877b0396d95c2a4aecb7f24605c32690802fbdf3
limbo <limbo_china@163.com>
2017-08-27 22:51:32 +0800

    update

--- www/models.py
+++ www/models.py
@@ -32,11 +32,8 @@ class Book(Model):
     __table__ = 'books'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
-    user_id = StringField(ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(50)')
-    user_image = StringField(ddl='varchar(500)')
     name = StringField(ddl='varchar(50)')
-    summary = StringField(ddl='varchar(200)')
+    author = StringField(ddl='varchar(200)')
     content = TextField()
     created_at = FloatField(default=time.time)
 
