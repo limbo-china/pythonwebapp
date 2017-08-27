29829ddf4ecb126ee0b5a34f4791cf6f4e530588
limbo <limbo_china@163.com>
2017-08-27 22:57:44 +0800

    update

---
+++ www/conf/config.py
@@ -0,0 +1,16 @@
+configs = {
+	'server':{
+		'host': '104.156.225.172',
+		'port': 80
+	},
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'aptx4869',
+        'database': 'pythonwebapp'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
